e88c2c4b45a4a3969d87ee13029f8b51d5fb7fc2
Md-Murthaza <murthazamurtu@gmail.com>
2025-01-05 14:18:46 +0530

    Added Serializers

--- Campaign_app/serializers.py
+++ Campaign_app/serializers.py
@@ -1,1 +1,102 @@
+from rest_framework import serializers
+from .models import  User, UserProfile, UserNotification, Agency, AgencySubscription, AgencyBilling, AgencyTeamMember
 
+
+# == User Serializer ==
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email','username','is_active','is_staff','is_superuser','last_login','date_joined']
+
+# == User Registration Serializer == 
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password_hash = serializers.CharField(write_only=True , min_length=8)
+    
+    class Meta:
+        model = User
+        fields = ['email','username','password_hash']
+
+    def validate_password(self,value):
+        if len(value)< 8:
+            raise serializers.ValidationError("Password must be at least 8 characters long. ")
+
+        return value
+    
+    def create(self,validated_data):
+        user = User.objects.create_user(
+            email = validated_data['email'],
+            password_hash = validated_data['password_hash'],
+            username = validated_data.get('username','')
+
+
+        )
+        return user
+
+
+# == UserProfile Serializer == 
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = UserProfile
+        fields = ['user','first_name','last_name','phone_number','profile_picture','time_zone','preferences','language','email_verified','phone_verified','last_password_change']
+
+        def update(self,instance,validated_data):
+            user_data = validated_data.pop('user',None)
+            if user_data:
+                for key, value  in user_data.items():
+                    setattr(instance.user,key, value)
+                instance.user.save()
+            return super().update(instance,validated_data)
+
+
+# == UserNotificationSeru=ializer == 
+
+class UserNotificationSerilaizer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = UserNotification
+        fields = ['user','type','message','is_read','created_at','metadata','priority']
+
+
+# == Agency Models == 
+
+class AgencySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Agency
+        fields = '__all__'
+
+# == Agencysubscription Serialzier == 
+
+class AgencySubscriptionSerializer(serializers.ModelSerializer):
+    agency = AgencySerializer()
+
+    class Meta:
+        model = AgencySubscription
+        fields = '__all__'
+
+
+#== AgencyBillingSerilaizer == 
+
+class AgencyBillingSerilaizer(serializers.ModelSerializer):
+    agency = AgencySerializer()
+
+    class Meta:
+        model = AgencyBilling
+        fields = '_all__'
+
+    
+#== AgencyTeamMemberSerialzier == 
+
+class AgencyTeamMemberSerializer(serializers.ModelSerializer):
+
+    agency = AgencySerializer()
+    user = UserSerializer()
+
+    class Meta:
+        model = AgencyTeamMember
+        fields = '__all__'
